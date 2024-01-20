9864e64c38ea26f7dacb350b78c6a8862cf74557
abeermosaad <abeermosaad69@gmail.com>
2024-01-20 19:04:28 +0200

    [Fix] Pycodestyle for task 101

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -6,7 +6,10 @@ T = TypeVar('T')
 # The types of the elements of the input are not know
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Optional[Union[T, None]] = None) -> Union[Any, T]:
+def safely_get_value(dct: Mapping,
+                     key: Any,
+                     default:
+                     Optional[Union[T, None]] = None) -> Union[Any, T]:
     """First element of a sequence"""
     if key in dct:
         return dct[key]
